6e7503d1799e31047712f43a85aecaf7a6ccebc8
Peperman66 <peperman66@seznam.cz>
2023-05-13 09:47:23 +0200

    add css support in MainWindow

--- windows/MainWindow.py
+++ windows/MainWindow.py
@@ -1,8 +1,16 @@
-from gi.repository import Gtk
+from gi.repository import Gtk, Gio, Gdk
+
+css_file = Gio.File.new_for_path("./css/main.css")
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, start_screen, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        screen = Gdk.Screen.get_default()
+        provider = Gtk.CssProvider()
+        style_context = Gtk.StyleContext()
+        style_context.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+        provider.load_from_file(css_file)
         self.current_screen = start_screen(self.change_screen)
         self.add(self.current_screen)
         self.current_screen.show()
