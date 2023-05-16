a0e88acfbe92fb4eae9a0aa41ffb4a8d54db14b3
Peperman66 <peperman66@seznam.cz>
2023-05-16 19:56:37 +0200

    add very basic off_screen

--- screens/off_screen.py
+++ screens/off_screen.py
@@ -8,10 +8,39 @@ class OffScreen(Gtk.Frame):
         self.__change_screen = change_screen
 
     def __add_elements(self):
-        on_button = Gtk.Button("Zapnout")
-        
-        from screens import OnScreen
-        on_button.connect("clicked", lambda x: self.__change_screen(OnScreen))
+        event_box = Gtk.EventBox()
+        box = Gtk.Box()
+        gytool_image = Gtk.Image()
+        srgh_image = Gtk.Image()
+        center_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
-        self.add(on_button)
-        on_button.show()
+        event_box.add(box)
+
+        box.pack_start(gytool_image, False, False, 0)
+        box.pack_start(center_box, True, False, 0)
+        box.pack_start(srgh_image, False, False, 0)
+
+        self.time_label = Gtk.Label("TIME")
+        self.date_label = Gtk.Label("DATE")
+        text_label = Gtk.Label("Stiskem zapnete učebnu...")
+
+        center_box.pack_start(self.time_label, False, False, 0)
+        center_box.pack_start(self.date_label, False, False, 0)
+        center_box.pack_start(text_label, False, False, 0)
+
+        self.add(event_box)
+        event_box.show()
+        box.show()
+        gytool_image.show()
+        center_box.show()
+        srgh_image.show()
+
+        text_label.show()
+        self.time_label.show()
+        self.date_label.show()
+
+        self.set_vexpand(True)
+        self.set_hexpand(True)
+
+        from screens import OnScreen
+        event_box.connect("button-press-event", lambda *args: self.__change_screen(OnScreen))
