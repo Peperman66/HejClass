91384d0889ab2ab4c348b5cdfd34107987a0f862
Peperman66 <peperman66@seznam.cz>
2023-05-16 19:56:52 +0200

    add very basic projector_frame

--- frames/projector_frame.py
+++ frames/projector_frame.py
@@ -6,6 +6,26 @@ class ProjectorFrame(Gtk.Frame):
         self.__add_elements()
 
     def __add_elements(self):
-        label = Gtk.Label("PROJECTOR")
-        label.show()
-        self.add(label)
+        grid = Gtk.Grid()
+        on_button = Gtk.Button("ZAP")
+        off_button = Gtk.Button("VYP")
+        mute_button = Gtk.Button("A/V MUTE")
+        freeze_button = Gtk.Button("FREEZE")
+        ports_button = Gtk.Button("PORTY")
+
+        grid.attach(on_button, 0, 0, 1, 1)
+        grid.attach(off_button, 0, 1, 1, 1)
+        grid.attach(mute_button, 1, 0, 1, 1)
+        grid.attach(freeze_button, 1, 1, 1, 1)
+        grid.attach(ports_button, 2, 0, 1, 2)
+
+        grid.set_row_homogeneous(True)
+        grid.set_column_homogeneous(True)
+
+        on_button.show()
+        off_button.show()
+        mute_button.show()
+        freeze_button.show()
+        ports_button.show()
+        grid.show()
+        self.add(grid)
