52778dd741e3d5e2ae2311d020d93c10302717ee
Peperman66 <peperman66@seznam.cz>
2023-05-25 22:09:50 +0200

    add class to upper frame in on screen

--- screens/on_screen.py
+++ screens/on_screen.py
@@ -6,26 +6,33 @@ class OnScreen(Gtk.Frame):
     def __init__(self, change_screen: Callable, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__change_screen = change_screen
+        self.other_screen = None
         self.__add_elements()
 
     def __change_other_screen(self, screen):
-        self.box.remove(self.other_screen)
-        self.other_screen.destroy()
+        if self.other_screen:
+            self.box.remove(self.other_screen)
+            self.other_screen.destroy()
         self.other_screen = screen()
         self.box.pack_start(self.other_screen, True, True, 0)
+
+        self.other_screen.get_style_context().add_class('upper-frame')
+
         self.other_screen.show()
 
     def __add_elements(self):
         from frames import HomeButtonRow, GeneralFrame
 
-        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.other_screen = GeneralFrame()
-        self.box.pack_start(self.other_screen, True, True, 0)
-        button_row = HomeButtonRow(self.__change_other_screen, self.__change_screen)
-        self.box.pack_end(button_row, False, False, 40)
+        self.get_style_context().add_class("borders")
 
-        self.other_screen.show()
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.box.props.name = "on-box"
+        button_row = HomeButtonRow(self.__change_other_screen, self.__change_screen)
+        self.box.pack_end(button_row, False, False, 0)
+
         button_row.show()
+
+        self.__change_other_screen(GeneralFrame)
 
         self.add(self.box)
         self.box.show()
