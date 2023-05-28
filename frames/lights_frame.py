3317f475ce2d9791e1a09713a127b4d3612a3c21
Peperman66 <peperman66@seznam.cz>
2023-05-28 21:08:08 +0200

    change layout of lights_frame

--- frames/lights_frame.py
+++ frames/lights_frame.py
@@ -1,4 +1,5 @@
 from gi.repository import Gtk
+from widgets import AspectButton
 
 class LightsFrame(Gtk.Frame):
     def __init__(self, *args, **kwargs):
@@ -13,42 +14,60 @@ class LightsFrame(Gtk.Frame):
         box.pack_end(grid, True, True, 10)
         box.pack_start(all_box, True, True, 10)
 
-        all_label = Gtk.Label("VŠE")
-        all_on = Gtk.Button("1")
-        all_off = Gtk.Button("0")
+        box.props.margin_top = 10
+        box.props.margin_bottom = 10
 
-        all_box.pack_start(all_label, True, True, 10)
-        all_box.pack_start(all_on, True, True, 10)
-        all_box.pack_start(all_off, True, True, 10)
+        all_label = Gtk.Label("VŠE")
+        all_on = AspectButton(1, "1")
+        all_off = AspectButton(1, "0")
+
+        all_on.props.margin_bottom = 20
+        all_off.props.margin_top = 20
+
+        all_label.get_style_context().add_class("text-medium")
+        all_on.get_style_context().add_class("text-xxl")
+        all_off.get_style_context().add_class("text-xxl")
+
+        all_box.pack_start(all_label, False, False, 0)
+        all_box.pack_start(all_on, True, True, 0)
+        all_box.pack_start(all_off, True, True, 0)
 
         all_box.props.spacing = 20
-        all_box.set_homogeneous(True)
 
         # Grid
         for i, text in enumerate(["LEVÁ ŘADA", "STŘEDNÍ ŘADA", "PRAVÁ ŘADA", "TABULE"]):
             label = Gtk.Label(text)
             grid.attach(label, i, 0, 1, 1)
+            label.get_style_context().add_class("text-medium")
             label.show()
 
         for i in range(4):
-            full = Gtk.Button("1")
-            none = Gtk.Button("0")
+            full = AspectButton(1, "1")
+            none = AspectButton(1, "0")
 
             if i < 3:
-                half = Gtk.Button("1/2")
+                half = AspectButton(1, "1/2")
                 grid.attach(half, i, 2, 1, 1)
+                half.set_vexpand(True)
+                half.get_style_context().add_class("text-xl")
                 half.show()
 
             grid.attach(full, i, 1, 1, 1)
             grid.attach(none, i, 3, 1, 1)
 
+            full.set_vexpand(True)
+            none.set_vexpand(True)
+
+            full.get_style_context().add_class("text-xl")
+            none.get_style_context().add_class("text-xl")
+
             full.show()
             none.show()
 
         grid.set_column_homogeneous(True)
-        grid.set_row_homogeneous(True)
+        grid.set_row_homogeneous(False)
         grid.set_row_spacing(20)
-        grid.set_column_spacing(20)
+        # grid.set_column_spacing(20)
         grid.set_vexpand(True)
         grid.set_hexpand(True)
 
